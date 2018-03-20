885f3a0f991acc197c2607dce413f9f23f0f5d55
Jon Ho <jonwho.contact@gmail.com>
2018-03-19 21:02:24 -0700

    update Gemfile so postgres compatible again

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 ruby '2.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.4'
+gem 'rails'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20170717233019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "todos", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "status"
+  create_table "todos", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
 end
