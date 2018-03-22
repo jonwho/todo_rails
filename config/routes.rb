a7360a1d97d1f6ec06297104b048a31e31636b6b
Jon Ho <jonwho.contact@gmail.com>
2018-03-22 02:06:15 -0700

    dont need checkbox if not signed in
    closes #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :todos, only: [:index, :create, :update, :destroy]
-
   root 'pages#home'
+
+  resources :todos, only: [:index, :create, :update, :destroy]
+  resources :sessions, only: [:create, :destroy]
+
 
   get 'auth/:provider/callback' => 'sessions#create'
   get 'auth/failure' => redirect('/')
   get 'signout' => 'sessions#destroy', as: 'signout'
-
-  resources :sessions, only: [:create, :destroy]
 end
